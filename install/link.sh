a5d3df26944614f21bc3d1442e6963e4a119d15c
Tolga Eren <tolgaeren@gmail.com>
2016-03-10 02:12:36 +0200

    final changes in dotfiles, fixed link.sh

--- install/link.sh
+++ install/link.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env zsh
 
-DOTFILES=$HOME/dotfiles
+DOTFILES=$HOME/git/dotfiles
 
 echo -e "\nCreating symlinks"
 echo "=============================="
-linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
-for file in $linkables ; do
+for file in $DOTFILES/*.symlink ; do
     target="$HOME/.$( basename $file ".symlink" )"
     if [ -e $target ]; then
         echo "~${target#$HOME} already exists... Skipping."
     else
-        echo "Creating symlink for $file"
+        echo "Creating symlink for $file at $target"
         ln -s $file $target
     fi
 done
